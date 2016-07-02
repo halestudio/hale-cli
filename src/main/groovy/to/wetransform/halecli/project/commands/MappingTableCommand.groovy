ac99fa740a87e021572eb2265623a0d75e1029df
Simon Templer <simon@wetransform.to>
2016-07-02 19:19:05 +0200

    fix mapping table command configuration

--- src/main/groovy/to/wetransform/halecli/project/commands/MappingTableCommand.groovy
+++ src/main/groovy/to/wetransform/halecli/project/commands/MappingTableCommand.groovy
@@ -3,15 +3,19 @@ package to.wetransform.halecli.project.commands;
 import java.net.URI;
 
 import eu.esdihumboldt.hale.common.align.io.AlignmentWriter
+import eu.esdihumboldt.hale.common.core.HalePlatform;
+import eu.esdihumboldt.hale.common.core.io.Value;
 import eu.esdihumboldt.hale.common.core.io.report.IOReport;
 import eu.esdihumboldt.hale.common.core.io.supplier.FileIOSupplier;
 import eu.esdihumboldt.hale.common.headless.impl.ProjectTransformationEnvironment
+import eu.esdihumboldt.hale.io.csv.writer.MappingTableConstants
 import eu.esdihumboldt.hale.io.xls.writer.XLSAlignmentMappingWriter
 import groovy.transform.CompileStatic;
 import groovy.util.OptionAccessor
 import to.wetransform.halecli.CommandContext
 import to.wetransform.halecli.Util;
 import to.wetransform.halecli.project.AbstractProjectCommand
+import static MappingTableConstants.*
 
 @CompileStatic
 class MappingTableCommand extends AbstractProjectCommand {
@@ -26,6 +30,13 @@ class MappingTableCommand extends AbstractProjectCommand {
     writer.serviceProvider = projectEnv
     writer.sourceSchema = projectEnv.sourceSchema
     writer.targetSchema = projectEnv.targetSchema
+    
+    writer.setContentType(HalePlatform.contentTypeManager.getContentType(
+      'eu.esdihumboldt.hale.io.xls.xlsx'))
+    
+    writer.setParameter(PARAMETER_MODE, MODE_BY_TYPE_CELLS as Value)
+    writer.setParameter(INCLUDE_NAMESPACES, false as Value)
+    writer.setParameter(TRANSFORMATION_AND_DISABLED_FOR, true as Value)
     
     //XXX only supported for files right now
     File projectFile = new File(projectLocation)
