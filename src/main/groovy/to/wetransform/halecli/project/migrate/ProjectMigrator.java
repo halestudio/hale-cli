a9edb8b707704edfaaf6524f17dde6226750f1d8
Simon Templer <simon@wetransform.to>
2024-08-20 17:19:36 +0200

    chore: apply common license headers

--- src/main/groovy/to/wetransform/halecli/project/migrate/ProjectMigrator.java
+++ src/main/groovy/to/wetransform/halecli/project/migrate/ProjectMigrator.java
@@ -1,3 +1,4 @@
+
 /*
  * Copyright (c) 2016 wetransform GmbH
  *
@@ -8,11 +9,7 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this distribution. If not, see <http://www.gnu.org/licenses/>.
- *
- * Contributors:
- *     wetransform GmbH <http://www.wetransform.to>
  */
-
 package to.wetransform.halecli.project.migrate;
 
 import java.util.Collections;
