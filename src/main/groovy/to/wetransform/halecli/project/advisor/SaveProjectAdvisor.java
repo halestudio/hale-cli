5cfb06d735666390bb88a4ff4c28b08d586a7e0c
Simon Templer <simon@wetransform.to>
2026-08-19 13:19:00 +0200

    refactor: determine skipped project files based on available advisors
    Instead of hard-coding the alignment as the only supported project
    file, look up each project file's save action at the project file
    extension point and check whether an I/O advisor is available for it
    (either the advisors managed by SaveProjectAdvisor or advisors
    registered at the advisor extension point). If an advisor for e.g. the
    styles.sld save action becomes available in the future it is used
    automatically. Project files that do not require an advisor are always
    kept.

--- src/main/groovy/to/wetransform/halecli/project/advisor/SaveProjectAdvisor.java
+++ src/main/groovy/to/wetransform/halecli/project/advisor/SaveProjectAdvisor.java
@@ -15,20 +15,26 @@ package to.wetransform.halecli.project.advisor;
 import java.net.URI;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.Platform;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import eu.esdihumboldt.hale.common.align.io.AlignmentIO;
+import de.fhg.igd.eclipse.util.extension.ExtensionObjectFactoryCollection;
+import de.fhg.igd.eclipse.util.extension.FactoryFilter;
 import eu.esdihumboldt.hale.common.align.model.Alignment;
 import eu.esdihumboldt.hale.common.core.HalePlatform;
 import eu.esdihumboldt.hale.common.core.io.IOAdvisor;
 import eu.esdihumboldt.hale.common.core.io.IOAdvisorRegister;
 import eu.esdihumboldt.hale.common.core.io.extension.IOAdvisorExtension;
+import eu.esdihumboldt.hale.common.core.io.extension.IOAdvisorFactory;
 import eu.esdihumboldt.hale.common.core.io.impl.AbstractIOAdvisor;
 import eu.esdihumboldt.hale.common.core.io.project.ProjectIO;
 import eu.esdihumboldt.hale.common.core.io.project.ProjectWriter;
+import eu.esdihumboldt.hale.common.core.io.project.extension.ProjectFileExtension;
 import eu.esdihumboldt.hale.common.core.io.project.model.AdvisorProjectFile;
 import eu.esdihumboldt.hale.common.core.io.project.model.Project;
 import eu.esdihumboldt.hale.common.core.io.project.model.ProjectFile;
@@ -75,10 +81,14 @@ public class SaveProjectAdvisor extends AbstractIOAdvisor<ProjectWriter>
     // only keep project files that can be saved in this context (i.e. that
     // have a save advisor available), e.g. styles.sld has none headlessly
     projectFiles.entrySet().removeIf(entry -> {
-      boolean supported = AlignmentIO.PROJECT_FILE_ALIGNMENT.equals(entry.getKey());
+      if (!(entry.getValue() instanceof AdvisorProjectFile)) {
+        // project file does not need an I/O advisor for saving
+        return false;
+      }
+      String saveActionId = findSaveActionId(entry.getKey());
+      boolean supported = saveActionId != null && hasSaveAdvisor(saveActionId);
       if (!supported) {
-        log.warn(
-            "Project file '{}' is not supported when saving projects with hale-cli and will be skipped.",
+        log.warn("Project file '{}' has no I/O advisor for saving available and will be skipped.",
             entry.getKey());
       }
       return !supported;
@@ -94,6 +104,55 @@ public class SaveProjectAdvisor extends AbstractIOAdvisor<ProjectWriter>
     if (projectLoadLocation != null) {
       provider.setPreviousTarget(projectLoadLocation);
     }
+  }
+
+  /**
+   * Determine the action ID for saving the project file with the given name, as
+   * declared at the project file extension point.
+   *
+   * @param fileName the project file name
+   * @return the save action ID or <code>null</code> if it cannot be determined
+   */
+  private String findSaveActionId(String fileName) {
+    for (IConfigurationElement element : Platform.getExtensionRegistry()
+        .getConfigurationElementsFor(ProjectFileExtension.ID)) {
+      if ("action-file".equals(element.getName())
+          && fileName.equals(element.getAttribute("name"))) {
+        IConfigurationElement[] save = element.getChildren("save");
+        if (save != null && save.length > 0) {
+          return save[0].getAttribute("action");
+        }
+      }
+    }
+    return null;
+  }
+
+  /**
+   * Determine if an I/O advisor for the given save action is available, either
+   * from the advisors managed here or from the advisor extension point.
+   *
+   * @param saveActionId the ID of the action for saving the project file
+   * @return if an advisor is available
+   */
+  private boolean hasSaveAdvisor(String saveActionId) {
+    if (advisors.containsKey(saveActionId)) {
+      return true;
+    }
+    List<IOAdvisorFactory> factories = IOAdvisorExtension.getInstance()
+        .getFactories(new FactoryFilter<IOAdvisor<?>, IOAdvisorFactory>() {
+
+          @Override
+          public boolean acceptFactory(IOAdvisorFactory factory) {
+            return saveActionId.equals(factory.getActionID());
+          }
+
+          @Override
+          public boolean acceptCollection(
+              ExtensionObjectFactoryCollection<IOAdvisor<?>, IOAdvisorFactory> collection) {
+            return true;
+          }
+        });
+    return factories != null && !factories.isEmpty();
   }
 
   @Override
